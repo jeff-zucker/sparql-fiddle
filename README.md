9e5b209eb315f517319cd0ab65c68849bd4a9cbb
Dub Zed <dubzed@gmail.com>
2018-12-09 11:36:40 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,223 @@
+# sparql-fiddle
+a way to play with SPARQL and Solid
+
+This app and accompanying library lets you use the SPARQL 
+query language from either an online form or the command line.  
+
+The online app is fairly self-explanatory, so please take a look at it
+to see the general operation.  Below is an explanation of the library
+that can be used in other apps.
+
+These notes on using the library are preliminary. I would REALLY
+like to hear comments about the API and ontology I talk about below.
+
+## Overview
+
+A fiddle, in the context of using sparql-fiddle as a coding library, is a data
+structure which specifies an RDF data source, a SPARQL  query, and a 
+results format.  The sparql-fiddle run() method runs the query against
+the data source and displays or returns the results in the specified format.
+The sparql-fiddle runFromLibrary() behaves similarly but retrieves the
+fiddle from a Turtle file library of fiddles.
+
+
+## Data and Query Sources
+
+The RDF data source may be a string containing Turtle or other rdflib
+parseable serialization, or it may be a URL pointing to a resource containing 
+the serialization.
+
+Likewise, the SPARQL query may be specified as a string containing the 
+query or a URL pointing to such a string.
+
+## Results as HTML or Text
+
+The format for results is stored in the "wanted" key and may be one of "HTML",  "Text", "Array", "Hash", "Value"
+
+An example:
+
+   const sf = require('sparql-fiddle') // or browser equivalent
+   let fiddle = {
+      data  : "http://example.com/myRDF",
+      query : "http://example.com/mySPARQL",
+     wanted : "HTML"
+   }
+   sf.run(fiddle).then( results => {
+       console.log(results)
+   }, err => console.log(err) )
+   // 
+   // output : an HTML table containing the results of the query
+
+The HTML format shows an HTML table of results.  Text shows fields
+one per line with a space between records.  If neither of these
+suits your purposes, you can ask for results as an Array or a Hash
+and format or process them any way you'd like by iterating over the structure.
+
+## Results as an Array
+
+The "Array" format returns an array of hashes (associative arrays). Given 
+"SELECT ?name ?addOn ...", the results would be something like
+
+  [ 
+     {"name":"Alu Gobi","addOn":"chutney" },
+     {"name":"Reuben Sandwich","addOn":"dill pickle" }
+  ]
+
+# Results as a Hash
+
+The Hash format returns a hash of hashes (associateve arrays). You need to
+specify a key before calling run().  If you specify fiddle.key="name" 
+the results would be something like this:
+
+  { 
+     "Alu Gobi"        : {"name":"Alu Gobi","addOn":"chutney" },
+     "Reuben Sandwich" : {"name":"Reuben Sandwich","addOn":"dill pickle" }
+  }
+ 
+The "Hash" format makes it very easy to read a Turtle config file into a
+hash object in a script.
+
+## Retrieving a Single Value
+
+The "Value" format returns a single value.  It is meant to work with a
+query that returns a single field in a single row.  If more than one row
+is retrieved, only the first will be examined.  If more than one field is
+retrieved, an arbitrary key will be returned.   Here's an example:
+
+    const sf = require('sparql-fiddle')
+    let fiddle = {
+        data:`@prefix : <http://schema.org/>. <> :name "hello world".`,
+       query:`PREFIX : <http://schema.org/> SELECT ?y WHERE {?x :name ?y .}`,
+      wanted:'Value'
+    }
+    sf.run(fiddle).then( res =>{ console.log(res) }, err => console.log(err) )
+    //
+    //  output : hello world
+
+## Default Results Format
+
+If no "wanted" key is supplied, the results format will default to "HTML" 
+in a browser context and "Text" in a node context.
+
+## Using N3 or RDF/XML 
+
+If the data source is not Turtle, the fiddle should specify the dataType
+as a mime content-type.
+
+    fiddle.dataType = "application/rdf+xml"
+
+
+## Row Handlers
+
+If you wish to munge the data before the results are returned, 
+specify a rowHandler function which will be applied to each row
+during processing
+
+    fiddle.rowHandler = function(row){
+        for(r in row){ row[r] = row[r].toUpperCase() }
+        return row
+    }
+    // values will be upper cased in results
+
+By default, run() accumulates all rows and then returns the accumulated
+results.  For very large results, this can eat up memory.  You can, instead,
+handle each row as it is processed without accumulating anything.  To do
+this, define a rowHandler which does what you need with each row but does
+not return anything.
+
+    fiddle.rowHandler = function(row){
+        for(r in row){ console.log(r + " : " + row[r] + "\n" }
+    }
+    // displays rows one at a time, as they are processed, accumulates nothing
+
+## Re-using a Store
+
+If you want to run multiple queries against the same data, specify the data as
+usual in the first run and set data to be an empty string in the following
+runs.  In this case, the rdflib store object will be reused, thus avoiding
+unnessary fetching and parsing which was already accomplished in the first
+run.
+
+## A Dummy Endpoint and No-Ontology Prototyping
+
+The dummy endpoint URL <http://example.org/inMemory#> is used as the 
+subject of the fiddle and is what the SPARQL searches.  You will see
+it listed if you have a query that selects the main subject.
+
+While ontologies are necessary for sharing data, it is sometimes useful
+to do without them when prototyping.  The turtle declaration 
+"@prefix : <#>." allows us to use on-the-fly classes and properties
+that we can later tie to an ontology.  This library supports the 
+same thing on the SPARQL side.  With sparql-fiddle, the URL 
+<http://example.org/inMemory#> can be used to refer to a data store
+which exists only temporarily.  We can make up classes and properties
+in the Turtle and refer to those made up entities in the SPARQL.  For 
+example:
+
+    data  :  @prefix : <#>. <> a :SomeDarnThing; :someRelation "hello world".
+
+    query :  PREFIX : <http://example.org/inMemory#> 
+             SELECT ?msg WHERE {
+                ?x a :SomeDarnThing; :someRelation ?msg .
+             }
+
+This fiddle will work even though the classes and properties it uses exist
+nowhere else.  NOTE : in this case "example.org" should be used literally,
+do not replace it.
+
+## An Ontology for shareable Fiddle Libraries
+
+Although I have not yet finalized the ontology, sparql-fiddle includes
+a way to store and share fiddles between Solid sources. So let's say we
+have a file "myLibrary.ttl" like this (ontology expressly omitted until 
+I finalized it):
+
+ @prefix : <#>
+ <> a :FiddleLibrary.
+ [] 
+  a :Fiddle;
+  :name "hello world";
+  :data """@prefix s: <http://schema.com/>. <> s:name "hello Solid world".""";
+  :query "PREFIX s: <http://schema.org/> SELECT ?msg WHERE {?x s:name ?msg.}"
+.
+
+We can now do this:
+
+    let fiddleLibrary = "http://example.org/myLibrary.ttl"
+    let options = {wanted:"Value"}
+    sf.runFiddle( fiddleLibrary, "hello world", options ).then( results => {
+        if( results === "hello Solid world" ) console.log("ok")
+    }, err => console.log(err) )
+
+The examples in the online app are all stored and retrieved from a
+fiddle library.  Once an ontology is finalized, anyone will be able
+to create and share fiddle libraries and the examples can grow with
+community contributions. And other uses for fiddle libraries will
+hopefully appear. They can be a kind of shared stored procedure library
+for the community.  To make these most useful, I propose some
+additional fields:
+
+    :contributor   # name/email 
+    :level         # beginner,intermediate,advance
+    
+    ... others? advice sought!
+
+## Caveats & Plans
+
+  * all SPARQL processing is based on rdflib.js which is not full SPARQL
+
+  * I have not yet implemented a secondary fetcher.  This means that 
+    the data endpoint is only the original URL and its fragments, the
+    library will not (yet) go on to fetch additional URLs listed in the 
+    original URL
+
+Enjoy!
+
+copyright (c) Jeff Zucker, 2018, released under MIT open source license
+
+
+
+
+
+
+
---
+++ sparql-fiddle.js
@@ -0,0 +1,243 @@
+if (typeof(module)!="undefined" && typeof($rdf)==="undefined")  $rdf = require('rdflib')
+
+/*
+{
+  data       : rdfStrOrUrl,
+  query      : sparqlStrOrUrl,
+  rdftype    : "text/turtle" (or other content-type parseable with rdflib.js),
+  results    : "Hash" or "Array" or "HTML" or "Text"
+  rowHandler : a function to handle rows of data
+}
+The first two are required, the next three may be omitted in which case their
+defaults are used:
+  rdftype defaults to "text/turtle"
+  results defaults to "HTML" in a browser context and "Text" in a node context
+  rowHandler defaults to undefined
+*/
+let SparqlFiddle = function(){
+    let self = this;
+    this.setRdfType = function(type){ this.rdfType = type }
+
+    this.do = function(fiddle){
+      return new Promise((resolve, reject)=>{
+        this.parseRdf(fiddle).then( response => {
+            this.prepare(fiddle).then( preparedQuery => {
+                this.execute(fiddle,preparedQuery).then( results => {
+                    self.store = fiddle.store 
+                    if(fiddle.wanted==="Array"){
+                        resolve(results)
+                    }
+                    else if(fiddle.wanted==="Hash") {
+                        resolve( self.ary2hash(fiddle,results) )
+                    }
+                    else if(results.length < 1) {
+                        resolve( "No results!"  )
+                    }
+                    else if(fiddle.wanted==="Value") {
+                        let key = ( Object.keys(results[0])[0]  )
+                        resolve( results[0][key] )
+                    }
+                    else { 
+                        let formatted = self.displayHandler(fiddle,results)
+                        resolve(formatted)
+                    }  
+                },err=>reject(err))
+            },err=>reject(err))
+        },err=>reject(err))
+      })
+    }
+    this.ary2hash = function(fiddle,ary){
+        let hash = {}
+        for(a in ary){
+            a = ary[a]
+            hash[a[fiddle.key]] = a
+        }
+        return hash
+    }
+    this.parseRdf = function(fiddle){ return new Promise((resolve, reject)=>{
+        if(!fiddle.data){
+              return resolve();
+        }
+        let type = fiddle.dataType || "text/turtle"
+        let endpointUrl = "http://example.org/inMemory"
+        try {
+            $rdf.parse(
+                fiddle.data, fiddle.store, $rdf.sym(endpointUrl).uri, type
+            )
+            resolve()
+        }
+        catch(err) { reject(err) }
+    })}
+    this.prepare = function(fiddle){
+        return new Promise((resolve, reject)=>{
+            try {
+              let query = $rdf.SPARQLToQuery(fiddle.query,false,fiddle.store)
+              resolve(query)
+            }
+            catch(err) { reject(err) }
+        })
+    }
+    this.execute =  function(fiddle,preparedQuery){
+        let rowHandler = fiddle.rowHandler || self.rowHandler
+        return new Promise((resolve, reject)=>{
+            let wanted = preparedQuery.vars
+            let resultAry = []
+            fiddle.store.query(preparedQuery, results =>  {
+                if(typeof(results)==="undefined") { reject("No results.") }
+                else { 
+                    let row = rowHandler(fiddle,wanted,results) 
+                    if(row) resultAry.push(row)
+                }
+            }, {} , function(){resolve(resultAry)} )
+        })
+    }
+    this.rowHandler = function(fiddle,wanted,results){
+        row = {}
+        for(r in results){
+            let found = false
+            let got = r.replace(/^\?/,'')
+            if(wanted.length){
+                for(w in wanted){
+                    if(got===wanted[w].label){ found=true; continue }
+                }
+                if(!found) continue
+            } 
+            row[got]=results[r].value
+        }
+        if(fiddle.rowHandler){
+            row = fiddle.rowHandler(row)
+        }
+        return(row)
+    }
+/*
+  DATA DISPLAY
+*/
+    this.displayHandler = function(fiddle,results){
+        let type = (fiddle.wanted)
+                 ? fiddle.wanted
+                 : (typeof(document)===undefined)
+                   ? "Text"
+                   : "HTML"
+        if(type==="Text") return self.showText(results)
+        if(type==="HTML") return self.showHtml(results)
+    }
+    this.showText = function(results){
+        let columnHeads = Object.keys(results[0]).reverse()
+        let str = "\n"
+        for(r in results){
+            let row = ""
+            for(k in columnHeads){
+                str += `${columnHeads[k]} : ${results[r][columnHeads[k]]}\n`
+            }
+            str += "\n"
+        }
+        return(str)
+    }
+    /* TBD : refactor to build a DOM object rather than a string
+    */
+    this.showHtml = function(results){
+        let columnHeads = Object.keys(results[0]).reverse()
+        let table = "<table>"
+        let topRow = ""
+        for(c in columnHeads){
+            topRow += `<th>${columnHeads[c]}</th>`
+        }
+        table += `<tr>${topRow}</tr>`
+        for(r in results){
+            let row = ""
+            for(k in columnHeads){
+                row += `<td>${results[r][columnHeads[k]]}</td>`
+            }
+            table += `<tr>${row}</tr>`
+        }
+        table += "</table>"
+        return(table)
+    }
+/* 
+  USER FUNCTIONS
+*/
+/*
+    PREFIX : <http://example.org/inMemory#>
+    SELECT ?name ?format ?data ?query WHERE { 
+        ?x :name ?name; :dataFormat ?format; :data ?data; :query ?query . 
+    }
+*/
+    this.runFromLibrary = function( fiddleLibrary, fiddleName, options){
+        return new Promise((resolve, reject)=>{
+            let fiddle = {
+              wanted : "Array",
+                data : fiddleLibrary,
+                query :`
+    PREFIX : <http://example.org/inMemory#>
+    SELECT ?type ?data ?query WHERE { 
+        ?x :name "${fiddleName}"; :dataFormat ?type; :data ?data; :query ?query . 
+    }
+`,
+            }
+            self.run( fiddle ).then( fiddle => {
+                let newFiddle = {
+                    wanted : options.wanted,
+                      data : fiddle[0].data,
+                     query : fiddle[0].query,
+                  dataType : fiddle[0].type
+                }
+                self.run( newFiddle ).then( results => {
+                    resolve(results)
+                }, err => reject(err) )
+            }, err => reject(err) )
+        }, err => reject(err) )
+    }
+    this.run = function(fiddle) { 
+      return new Promise((resolve, reject)=>{
+        fiddle.store = (fiddle.data.length>0)
+                     ? $rdf.graph()
+                     : self.store
+        if( fiddle.data.match(/^http/ ) ){
+            self.loadFromUrl(fiddle,"data").then( fiddle => {
+                self.loadSparqlAndDo( fiddle ).then( results => {
+                    resolve(results)
+                }, err => reject(err) )
+            }, err => reject(err) )
+        }
+        else {
+            self.loadSparqlAndDo( fiddle ).then( results => {
+                resolve(results)
+            }, err => reject(err) )
+        }
+      })
+    }
+/* 
+  DATA LOADING
+*/
+    this.loadSparqlAndDo = function( fiddle ) {
+      return new Promise((resolve, reject)=>{
+        if( fiddle.query.match( /^http/ ) ){
+            this.loadFromUrl(fiddle,"query").then( fiddle => {
+                self.do(fiddle).then( results => {
+                    resolve(results)
+                }, err => reject(err) )
+            }, err =>  reject(err) )
+        }
+        else {
+            this.do(fiddle).then( results => {
+                resolve(results)
+            }, err => reject(err) )
+        }
+      })
+    }
+    this.loadFromUrl = function(fiddle,type){
+      let url = fiddle[type]
+      return new Promise((resolve, reject)=>{
+        let fetcher = new $rdf.fetcher( $rdf.graph() );
+        try {
+            fetcher.load(url).then( response => {
+                // replace the url with it's content
+                fiddle[type] = response.responseText
+                resolve( fiddle )
+            })
+        } catch(err) { reject(err) }
+      })
+    }
+    return this;
+}
+if (typeof(module)!="undefined" )  module.exports = SparqlFiddle()
